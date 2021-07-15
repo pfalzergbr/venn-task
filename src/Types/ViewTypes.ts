bd7d6d555b744ec07cd34e00de0e08ad8b509771
Gabor Pfalzer <pfalzer@gmail.com>
2021-07-15 14:50:43 +0100

    extract attribute types in ViewTypes to use in createVTextBox

--- src/Types/ViewTypes.ts
+++ src/Types/ViewTypes.ts
@@ -13,6 +13,7 @@ export type LinkType = {
   payload: string;
   type: string;
 };
+export type ViewTypes = IVTextBox | IVImageWithPadding | IVImageCarousel;
 
 export interface IView {
   moduleType: ModuleType;
@@ -20,37 +21,41 @@ export interface IView {
   // Added field for the reducer, to be able to mark multiple fields for deletion.
 }
 
-export type ViewTypes = IVTextBox | IVImageWithPadding | IVImageCarousel;
+export interface VTextBoxAttributes {
+  padding: number;
+  backgroundColor: BackgroundColorType;
+  bodyText: string;
+  textAlignment: TextAlignmentType;
+  fontSize: number;
+  capitalised: boolean;
+  fontColor: FontcolorType;
+}
+
+export interface VImageWithPaddingAttributes {
+  padding: number;
+  backgroundColor: BackgroundColorType;
+  imageUrl: string;
+  link: LinkType;
+}
+
+export interface VImageCarouselAttributes {
+  padding: number;
+  images: string[];
+}
 
 export interface IVTextBox extends IView {
   moduleType: 'VTextBox';
-  attributes: {
-    padding: number;
-    backgroundColor: BackgroundColorType;
-    bodyText: string;
-    textAlignment: TextAlignmentType;
-    fontSize: number;
-    capitalised: boolean;
-    fontColor: FontcolorType;
-  };
+  attributes: VTextBoxAttributes;
 }
 
 export interface IVImageWithPadding {
   moduleType: 'VImageWithPadding';
-  attributes: {
-    padding: number;
-    backgroundColor: BackgroundColorType;
-    imageUrl: string;
-    link: LinkType;
-  };
+  attributes: VImageWithPaddingAttributes;
   heightMultiplier: number;
 }
 
 export interface IVImageCarousel {
   moduleType: 'VImageCarousel';
-  attributes: {
-    padding: number;
-    images: string[];
-  };
+  attributes: VImageCarouselAttributes;
   heightMultiplier: number;
 }
