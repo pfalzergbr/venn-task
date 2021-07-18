faf5dfd3ac264010848c1e421520dd893a81500f
Gabor Pfalzer <pfalzer@gmail.com>
2021-07-18 15:58:39 +0100

    style footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,7 +1,9 @@
+import styles from './styles/Footer.module.css';
+
 // Component to render the footer
 const Footer: React.FC = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p>Created for Venn Apps Frontend Challenge by Gabor Pfalzer</p>
       <a href="https://pfalzer.dev">pfalzer.dev</a>
     </footer>
